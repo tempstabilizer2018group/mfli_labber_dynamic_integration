860c16aef3d1b5ddfa6fda938e6cce01e050ffc8
Hans Maerki <hansm@maerki-lenovo-330>
2019-03-30 19:15:40 +0100

    first try

--- Drivers/ZI_MFLI_Lockin/ZI_MFLI_Lockin.py
+++ Drivers/ZI_MFLI_Lockin/ZI_MFLI_Lockin.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python
 
-from VISA_Driver import VISA_Driver
+import os
+import sys
 
-class Driver(VISA_Driver):
-    """ The SRS 830 driver re-implements the VISA driver with extra options"""
+import InstrumentDriver
+
+__version__  = '0.9'
+
+
+import ZI_MFLI_lib
+
+class Driver(InstrumentDriver.InstrumentWorker):
+    """ This class wraps the ziPython API"""
+    def performOpen(self, options={}):
+        self.x, self.y, self.r, self.theta = 0.0, 0.0, 0.0, 0.0
+
+
+        self.log("A: " + self.comCfg.address, level=30)
+
+        self.ziDevice = ZI_MFLI_lib.Zi_Device(self.comCfg.address)
+
+        self.ziDevice.init_mfli_lock_in()
+
+    def performClose(self, bError=False, options={}):
+        """Perform the close instrument connection operation"""
+        self.ziDevice.disconnect()
+
+    def performSetValue(self, quant, value, sweepRate=0.0, options={}):
+        return 0.0
 
     def performGetValue(self, quant, options={}):
-        """Perform the Get Value instrument operation"""
-        # perform special getValue for reading complex value
-        name = str(quant.name)
-        if name == 'Value':
-            # get complex value in one instrument reading
-            sCmd = 'SNAP?1,2'
-            sAns = self.askAndLog(sCmd).strip()
-            lData =  sAns.split(',')
-
-            #Sometimes, we receive the value twice
-            #(0.12e-3,4.56e-70.12e-3,4.56e-7 instead of 0.12e-3,4.56e-7)
-            #This is a simple fix:
-            if len(lData) > 2:
-                lData =  sAns[:int(len(sAns)/2)].split(',')
-            #Also, sometimes we receive an additional "-" at the end of a value
-            #(0.12e-3,4.56e-7- instead of 0.12e-3,4.56e-7)
-            #Hence, another simple fix:
-            if lData[1][-1] == "-":
-                lData[1] = lData[1][:-1]
-
-            # return complex values
-            return complex(float(lData[0].strip()), float(lData[1].strip()))
-        else:
-            # run the generic visa driver case
-            return VISA_Driver.performGetValue(self, quant, options=options)
-
-
-if __name__ == '__main__':
-    pass
+        if quant.name in ['X',]:
+            self.x, self.y, self.r, self.theta = self.ziDevice.get_lockin()
+            return self.x
+        if quant.name in ['Y',]:
+            return self.y
+        if quant.name in ['R',]:
+            return self.r
+        if quant.name in ['theta',]:
+            return self.theta
